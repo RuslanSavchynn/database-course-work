77f009ae8609615b931ab00d627b816a4b65f4ec
Ruslannsvv <ruslannsvvv@gmail.com>
2024-10-27 18:34:22 +0200

    Started work with questions with OpenAI GPT

---
+++ src/app/api/questions/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server"
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { ZodError } from "zod";
+import { strict_output } from "@/lib/gpt";
+// /api/questions
+export const POST = async (req: Request, res: Response) => {
+
+    try {
+
+        const body = await req.json();
+        const { amount, topic, type } = quizCreationSchema.parse(body);
+        let questions: any;
+        
+        if (type === "open_ended") {
+            questions = await strict_output(
+                "You are a helpful AI that is able to generetate a pair of question and answers, the length of the answer should not exeed 15 words, store all the pairs of answers and questions in a JSON array",
+                'You are to generate a random hard open-ended question about ${topic}',
+                {
+                    question: "question",
+                    answer: "answer with max length of 15 words",
+                }
+            );
+        }
+
+        return NextResponse.json(
+            {
+                questions,
+            },
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        if (error instanceof ZodError) {
+            return NextResponse.json({
+                error: error.issues,
+            },
+                {
+
+                    status: 400,
+
+                }
+            )
+        }
+    }
+
+}
