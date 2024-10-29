58902c88a0c328d93178ca7252833b11748b993a
Ruslannsvv <ruslannsvvv@gmail.com>
2024-10-29 18:15:18 +0200

    Fix bugs in UserAccountNav.tsx

--- src/components/ui/UserAccountNav.tsx
+++ src/components/ui/UserAccountNav.tsx
@@ -30,33 +30,31 @@ const UserAccountNav = ({ user }: Props) => {
           }}
         />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-white" align="end">
+      <DropdownMenuContent className="bg-white dark:bg-gray-800" align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.name && <p className="font-medium dark:text-white">{user.name}</p>}
             {user.email && (
-              <p className="w-[200px] truncate text-sm text-zinc-700">
+              <p className="w-[200px] truncate text-sm text-zinc-700 dark:text-zinc-300">
                 {user.email}
               </p>
             )}
           </div>
         </div>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator className="bg-zinc-200 dark:bg-zinc-600" />
         <DropdownMenuItem asChild>
-          <Link href="/">Meow</Link>
+          <Link href="/" className="dark:text-white">Meow</Link>
         </DropdownMenuItem>
-
-        <DropdownMenuSeparator />
-
+        <DropdownMenuSeparator className="bg-zinc-200 dark:bg-zinc-600" />
         <DropdownMenuItem
           onSelect={(event) => {
             event.preventDefault();
             signOut().catch(console.error);
           }}
-          className="text-red-600 cursor-pointer"
+          className="text-red-600 cursor-pointer dark:text-red-400"
         >
           Sign out
-          <LogOut className="w-4 h-4 ml-2 " />
+          <LogOut className="w-4 h-4 ml-2" />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
