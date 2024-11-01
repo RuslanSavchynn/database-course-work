56bc76df87b6594687e18100c9ece9154182fc65
Ruslannsvv <ruslannsvvv@gmail.com>
2024-11-01 20:21:45 +0200

    Statistics Questions List

--- src/app/statistics/[gameId]/page.tsx
+++ src/app/statistics/[gameId]/page.tsx
@@ -1,4 +1,5 @@
 import AccuracyCard from '@/components/statistics/AccuracyCard'
+import QuestionList from '@/components/statistics/QuestionList'
 import ResultsCard from '@/components/statistics/ResultsCard'
 import TimeTakenCard from '@/components/statistics/TimeTakenCard'
 import { buttonVariants } from '@/components/ui/button'
@@ -22,14 +23,35 @@ const StatisticsPage = async ({ params: { gameId } }: Props) => {
     }
 
     const game = await prisma.game.findUnique({
-        where: { id: gameId }
+        where: { id: gameId },
+        include: {
+            questions: true
+        }
     })
     if (!game) {
         return redirect('/quiz')
     }
 
+    let accuracy: number = 0
+    if (game.gameType == 'mcq') {
+        let totalCorrect = game.questions.reduce((acc, question) => {
+            if (question.isCorrect) {
+                return acc + 1
+            }
+            return acc
+        }, 0)
 
+        accuracy = (totalCorrect / game.questions.length) * 100
+    } else if (game.gameType === 'open_ended') {
+        let totalPercentage = game.questions.reduce((acc, question) => {
+            return acc + (question.percentageCorrect || 0)
 
+        }, 0)
+        accuracy = (totalPercentage / game.questions.length)
+
+    }
+
+    accuracy = Math.round(accuracy * 100) / 100
     return (
         <>
             <div className='p-8 mx-auto max-w-7xl'>
@@ -43,12 +65,14 @@ const StatisticsPage = async ({ params: { gameId } }: Props) => {
                     </div>
                 </div>
                 <div className='grid gap-4 mt-4 md:grid-cols-7'>
-                     <ResultsCard accuracy={80} />
-                    <AccuracyCard accuracy={80} />
-                    <TimeTakenCard timeEnded={new Date()} timeStarted={new Date()} /> 
+                    <ResultsCard accuracy={accuracy} />
+                    <AccuracyCard accuracy={accuracy} />
+                    <TimeTakenCard
+                         timeEnded={new Date()}
+                        timeStarted={game.timeStarted} />
                 </div>
 
-                {/* <QuestionList/> */}
+                <QuestionList questions={game.questions}/>
             </div>
         </>
     )
